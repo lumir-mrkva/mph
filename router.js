4c07718ca2bab1f3cf14812d6bec3ad824c2aada
Lumir Mrkva <mrq@mrq.cz>
2014-10-02 05:16:43 +0200

    basic routing

---
+++ router.js
@@ -0,0 +1,18 @@
+Router.route('hello', {
+	path: '/'
+});
+
+Router.route('create', {
+	path: '/polls'
+});
+
+Router.route('vote', {
+	path: '/polls/:_id',
+	data: function() {
+		return {id: this.params._id};
+	}
+});
+
+Router.route('results', {
+	path: '/polls/:_id/results'
+});
